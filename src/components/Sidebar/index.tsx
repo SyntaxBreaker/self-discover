29d0e06253f0882dd2b48704c0e872b068bb0266
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-07-26 12:01:03 +0200

    Improve sidebar appearance

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,9 @@
 import {
   Box,
   Flex,
-  useDisclosure,
   Text,
   Tooltip,
   Link,
-  HStack,
   Icon,
   Divider,
 } from "@chakra-ui/react";
@@ -65,18 +63,18 @@ function Header() {
           </Box>
         </Link>
       </Tooltip>
+      <Divider position="absolute" bottom={14} />
       <Tooltip label="Your profile" display={{ base: "initial", sm: "none" }}>
-        <Link href="/profile" position="absolute" bottom={0} width="100%">
+        <Link href="/profile" position="absolute" bottom={0} width='100%'>
           <Box
             paddingY={2}
             paddingX={{ base: 0, sm: 2 }}
-            backgroundColor="gray.600"
-            _hover={{ backgroundColor: "gray.800" }}
+            _hover={{ backgroundColor: "gray.700" }}
             display="flex"
             alignItems="center"
             gap={2}
           >
-            <Icon viewBox="0 0 24 24" boxSize={10} color="white">
+            <Icon viewBox="0 0 24 24" boxSize={10} color="white" margin={{base: '0 auto', sm: 'initial'}}>
               <path
                 fill="currentColor"
                 d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88a9.947 9.947 0 0 1 12.28 0C16.43 19.18 14.03 20 12 20z"
