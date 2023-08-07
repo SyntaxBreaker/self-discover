ebd94d92b30b4543fc21de05695c3922f6a8e0ff
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-08-07 15:38:30 +0200

    Add buttons to remove and edit articles

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,14 +1,25 @@
 import { useLoaderData } from "react-router-dom";
 import IArticle from "../../types/article";
-import { Badge, Box, Container, Stack, Text } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import ChakraUIRenderer from "chakra-ui-markdown-renderer";
+import { useAuth } from "../../context/AuthProvider";
+import { IAuthContext } from "../../types/auth";
 
 function Article() {
   const { article, error } = useLoaderData() as {
     article: IArticle;
     error: string;
   };
+  const { user } = useAuth() as IAuthContext;
 
   return (
     <Container maxW={{ base: "100%", md: "50%" }} py={8}>
@@ -20,12 +31,26 @@ function Article() {
         </Box>
       ) : (
         <Box>
-          <Text fontSize="6xl" fontWeight="extrabold">
-            {article.title}
-          </Text>
-          <Text marginTop={0}>
-            {article.created_at.split("T")[0].split("-").reverse().join(".")}
-          </Text>
+          <Stack direction="row" justify="space-between" wrap="wrap">
+            <Stack>
+              <Heading as="h1" size="2xl">
+                {article.title}
+              </Heading>
+              <Text>
+                {article.created_at
+                  .split("T")[0]
+                  .split("-")
+                  .reverse()
+                  .join(".")}
+              </Text>
+            </Stack>
+            {user && user.id === article.authorId && (
+              <Stack direction="row" padding={2}>
+                <Button size="lg">Edit</Button>
+                <Button size="lg">Remove</Button>
+              </Stack>
+            )}
+          </Stack>
           <Box
             as={ReactMarkdown}
             components={ChakraUIRenderer()}
@@ -35,7 +60,7 @@ function Article() {
             {article.content}
           </Box>
           {article.tags && (
-            <Stack direction="row" marginTop={12}>
+            <Stack direction="row" marginTop={8} wrap="wrap">
               {article.tags.map((tag) => (
                 <Badge colorScheme="blue" padding={2} key={tag}>
                   {tag}
