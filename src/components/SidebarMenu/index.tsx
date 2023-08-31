54456a1c2d4f1f1eb1850ca69e283b5011d7f025
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-08-31 15:11:06 +0200

    Add user avatar to SidebarMenu if available

--- src/components/SidebarMenu/index.tsx
+++ src/components/SidebarMenu/index.tsx
@@ -7,6 +7,7 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
+  Image,
 } from "@chakra-ui/react";
 import { User } from "@supabase/supabase-js";
 import { NavLink } from "react-router-dom";
@@ -28,17 +29,27 @@ function SidebarMenu({ user }: { user: User }) {
         isTruncated
       >
         <Flex alignItems="center" gap={2}>
-          <Icon
-            viewBox="0 0 24 24"
-            boxSize={10}
-            color="white"
-            margin={{ base: "0 auto", sm: "initial" }}
-          >
-            <path
-              fill="currentColor"
-              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88a9.947 9.947 0 0 1 12.28 0C16.43 19.18 14.03 20 12 20z"
+          {user.user_metadata.avatar_url ? (
+            <Image
+              src={user.user_metadata.avatar_url}
+              alt="avatar"
+              boxSize={10}
+              borderRadius="full"
+              objectFit="cover"
             />
-          </Icon>
+          ) : (
+            <Icon
+              viewBox="0 0 24 24"
+              boxSize={10}
+              color="white"
+              margin={{ base: "0 auto", sm: "initial" }}
+            >
+              <path
+                fill="currentColor"
+                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88a9.947 9.947 0 0 1 12.28 0C16.43 19.18 14.03 20 12 20z"
+              />
+            </Icon>
+          )}
           <Text
             color="white"
             isTruncated
