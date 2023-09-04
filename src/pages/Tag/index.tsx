14d2d7ebc9867db5a7b0d7aec7c8d9b9a7b2f1c2
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-09-04 13:37:03 +0200

    Add heading to tag page

--- src/pages/Tag/index.tsx
+++ src/pages/Tag/index.tsx
@@ -1,17 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import IArticle from "../../types/article";
 import { PostgrestError } from "@supabase/supabase-js";
 import ArticleList from "../../components/ArticleList";
 import ResponsiveContainer from "../../components/ResponsiveContainer";
+import { Heading } from "@chakra-ui/react";
 
 function Tag() {
   const { articles, error } = useLoaderData() as {
     articles: IArticle[];
     error: PostgrestError | null;
   };
+  const { tag } = useParams();
 
   return (
     <ResponsiveContainer>
+      <Heading textAlign="center" as="h1" marginBottom={8}>
+        Explore {tag}
+      </Heading>
       <ArticleList articles={articles} error={error} />
     </ResponsiveContainer>
   );
