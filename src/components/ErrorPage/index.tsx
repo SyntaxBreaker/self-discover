86d2624e2cc20990da8193eaf887d28a03a02fd7
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-09-06 14:08:03 +0200

    Implement back button in ErrorPage component

--- src/components/ErrorPage/index.tsx
+++ src/components/ErrorPage/index.tsx
@@ -1,14 +1,26 @@
-import { Container, Heading, Text } from '@chakra-ui/react';
-import {useRouteError} from 'react-router-dom';
+import { Button, Container, Heading, Text } from "@chakra-ui/react";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error: any = useRouteError();
-  
+  const navigate = useNavigate();
+
   return (
-    <Container maxW='100%' textAlign="center" py={8}>
+    <Container textAlign="center" py={8}>
       <Heading>Oops!</Heading>
-      <Text marginTop={4}>Sorry, an unexpected error has occured.</Text>
-      <Text marginTop={4} color="gray.500">{error?.statusText || error?.message}</Text>
+      <Text marginTop={4} color="gray.500">
+        Sorry, an unexpected error has occured.
+      </Text>
+      <Text marginTop={4} color="gray.900">
+        {error?.statusText || error?.message}
+      </Text>
+      <Button
+        marginTop={4}
+        colorScheme="red"
+        onClick={() => navigate(-1)}
+      >
+        Go back
+      </Button>
     </Container>
-  )
+  );
 }
