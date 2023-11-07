ce55fdb2f10bdf0cb140d4b3687631000b4d7bf3
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2023-11-07 11:27:47 +0100

    Improve error handling in ArticleList

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -1,9 +1,10 @@
-import { Alert, AlertIcon, SimpleGrid, Stack } from "@chakra-ui/react";
+import { SimpleGrid, Stack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import ArticleCard from "../ArticleCard";
 import IArticle from "../../types/article";
 import { PostgrestError } from "@supabase/supabase-js";
 import EmptyArticleList from "../EmptyArticleList";
+import Error from "../Error";
 
 function ArticleList({
   articles,
@@ -15,10 +16,7 @@ function ArticleList({
   return (
     <Stack marginTop={8}>
       {error ?
-        <Alert status="error" padding={4} borderRadius={8}>
-          <AlertIcon />
-          Unable to load articles. Please check your internet connection.
-        </Alert>
+        <Error errorMessage="Unable to load articles." />
         : articles?.length === 0 ?
           <EmptyArticleList />
           :
