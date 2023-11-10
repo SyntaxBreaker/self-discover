ee41e9c98f95ffe6a054170c236b5d8f88132f7b
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2023-11-10 11:01:43 +0100

    Improve breakpoints in ResponsiveContainer

--- src/components/ResponsiveContainer/index.tsx
+++ src/components/ResponsiveContainer/index.tsx
@@ -2,7 +2,10 @@ import { Container } from "@chakra-ui/react";
 
 function ResponsiveContainer({ children }: { children: React.ReactNode }) {
   return (
-    <Container maxW={{ base: "100%", md: "75%", lg: "50%" }} py={8}>
+    <Container
+      maxW={{ base: "100%", md: "75%", lg: "65%", "2xl": "55%" }}
+      py={8}
+    >
       {children}
     </Container>
   );
