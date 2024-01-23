b1b8d26a86461afa5fefefc546273eb8beb2cd6d
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-01-23 12:20:03 +0100

    Improve sidebar navigation

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -32,143 +32,148 @@ function Sidebar() {
       alignItems={{ base: "center", lg: "inherit" }}
       position="sticky"
       top="0"
+      justifyContent="space-between"
     >
-      <Tooltip label="Homepage" display={{ base: "initial", lg: "none" }}>
-        <Link as={NavLink} to="/" _activeLink={{ fontWeight: "900" }}>
-          <Box
-            paddingY={2}
-            paddingX={{ base: 0, sm: 2 }}
-            _hover={{ backgroundColor: "gray.700" }}
-            display="flex"
-            alignItems="center"
-            gap={2}
-          >
-            <Icon as={RiMentalHealthFill} />
-            <Text color="white" display={{ base: "none", lg: "inline" }}>
-              SelfDiscover
-            </Text>
-          </Box>
-        </Link>
-      </Tooltip>
-      <Divider />
-      <Tooltip label="Create Post" display={{ base: "initial", lg: "none" }}>
-        <Link as={NavLink} to="/create" _activeLink={{ fontWeight: "900" }}>
-          <Box
-            paddingY={2}
-            paddingX={{ base: 0, sm: 2 }}
-            _hover={{ backgroundColor: "gray.700" }}
-            display="flex"
-            alignItems="center"
-            gap={2}
-          >
-            <Icon as={MaterialSymbolsAdd} />
-            <Text color="white" display={{ base: "none", lg: "inline" }}>
-              Create Article
-            </Text>
-          </Box>
-        </Link>
-      </Tooltip>
-      <Tooltip label="Events" display={{ base: "initial", lg: "none" }}>
-        <Link as={NavLink} to="/events" _activeLink={{ fontWeight: "900" }}>
-          <Box
-            paddingY={2}
-            paddingX={{ base: 0, sm: 2 }}
-            _hover={{ backgroundColor: "gray.700" }}
-            display="flex"
-            alignItems="center"
-            gap={2}
-          >
-            <Icon as={MaterialSymbolsDateRange} />
-            <Text color="white" display={{ base: "none", lg: "inline" }}>
-              Events
-            </Text>
-          </Box>
-        </Link>
-      </Tooltip>
-      <Tooltip label="Tags" display={{ base: "initial", lg: "none" }}>
-        <Link as={NavLink} to="/tags" _activeLink={{ fontWeight: "900" }}>
-          <Box
-            paddingY={2}
-            paddingX={{ base: 0, sm: 2 }}
-            _hover={{ backgroundColor: "gray.700" }}
-            display="flex"
-            alignItems="center"
-            gap={2}
-          >
-            <Icon as={TeenyiconsHashtagOutline} />
-            <Text color="white" display={{ base: "none", lg: "inline" }}>
-              Tag List
-            </Text>
-          </Box>
-        </Link>
-      </Tooltip>
-      <Tooltip label="Chat" display={{ base: "initial", lg: "none" }}>
-        <Link as={NavLink} to="/chat" _activeLink={{ fontWeight: "900" }}>
-          <Box
-            paddingY={2}
-            paddingX={{ base: 0, sm: 2 }}
-            _hover={{ backgroundColor: "gray.700" }}
-            display="flex"
-            alignItems="center"
-            gap={2}
-          >
-            <Icon as={JamMessagesF} />
-            <Text color="white" display={{ base: "none", lg: "inline" }}>
-              Chat
-            </Text>
-          </Box>
-        </Link>
-      </Tooltip>
-      <Tooltip label="FAQ" display={{ base: "initial", lg: "none" }}>
-        <Link as={NavLink} to="/faq" _activeLink={{ fontWeight: "900" }}>
-          <Box
-            paddingY={2}
-            paddingX={{ base: 0, sm: 2 }}
-            _hover={{ backgroundColor: "gray.700" }}
-            display="flex"
-            alignItems="center"
-            gap={2}
-          >
-            <Icon as={MdiFrequentlyAskedQuestions} />
-            <Text color="white" display={{ base: "none", lg: "inline" }}>
-              FAQ
-            </Text>
-          </Box>
-        </Link>
-      </Tooltip>
-      <Divider position="absolute" bottom={14} />
-      <Box position="absolute" bottom={0} width="100%">
-        {user ? (
-          <SidebarMenu user={user} />
-        ) : (
-          <Tooltip label="Sign in" display={{ base: "initial", lg: "none" }}>
-            <Link as={NavLink} to="/signIn" _activeLink={{ fontWeight: "900" }}>
-              <Box
-                paddingY={2}
-                paddingX={{ base: 0, sm: 2 }}
-                _hover={{ backgroundColor: "gray.700" }}
-                display="flex"
-                alignItems="center"
-                gap={2}
-              >
-                <Icon
-                  viewBox="0 0 24 24"
-                  boxSize={10}
-                  color="white"
-                  margin={{ base: "0 auto", sm: "initial" }}
+      <Box>
+        <Tooltip label="Homepage" display={{ base: "initial", lg: "none" }}>
+          <Link as={NavLink} to="/" _activeLink={{ fontWeight: "900" }}>
+            <Box
+              paddingY={2}
+              paddingX={{ base: 0, sm: 2 }}
+              _hover={{ backgroundColor: "gray.700" }}
+              display="flex"
+              alignItems="center"
+              gap={2}
+            >
+              <Icon as={RiMentalHealthFill} />
+              <Text color="white" display={{ base: "none", lg: "inline" }}>
+                SelfDiscover
+              </Text>
+            </Box>
+          </Link>
+        </Tooltip>
+        <Divider />
+        <Tooltip label="Create Post" display={{ base: "initial", lg: "none" }}>
+          <Link as={NavLink} to="/create" _activeLink={{ fontWeight: "900" }}>
+            <Box
+              paddingY={2}
+              paddingX={{ base: 0, sm: 2 }}
+              _hover={{ backgroundColor: "gray.700" }}
+              display="flex"
+              alignItems="center"
+              gap={2}
+            >
+              <Icon as={MaterialSymbolsAdd} />
+              <Text color="white" display={{ base: "none", lg: "inline" }}>
+                Create Article
+              </Text>
+            </Box>
+          </Link>
+        </Tooltip>
+        <Tooltip label="Events" display={{ base: "initial", lg: "none" }}>
+          <Link as={NavLink} to="/events" _activeLink={{ fontWeight: "900" }}>
+            <Box
+              paddingY={2}
+              paddingX={{ base: 0, sm: 2 }}
+              _hover={{ backgroundColor: "gray.700" }}
+              display="flex"
+              alignItems="center"
+              gap={2}
+            >
+              <Icon as={MaterialSymbolsDateRange} />
+              <Text color="white" display={{ base: "none", lg: "inline" }}>
+                Events
+              </Text>
+            </Box>
+          </Link>
+        </Tooltip>
+        <Tooltip label="Tags" display={{ base: "initial", lg: "none" }}>
+          <Link as={NavLink} to="/tags" _activeLink={{ fontWeight: "900" }}>
+            <Box
+              paddingY={2}
+              paddingX={{ base: 0, sm: 2 }}
+              _hover={{ backgroundColor: "gray.700" }}
+              display="flex"
+              alignItems="center"
+              gap={2}
+            >
+              <Icon as={TeenyiconsHashtagOutline} />
+              <Text color="white" display={{ base: "none", lg: "inline" }}>
+                Tag List
+              </Text>
+            </Box>
+          </Link>
+        </Tooltip>
+        <Tooltip label="Chat" display={{ base: "initial", lg: "none" }}>
+          <Link as={NavLink} to="/chat" _activeLink={{ fontWeight: "900" }}>
+            <Box
+              paddingY={2}
+              paddingX={{ base: 0, sm: 2 }}
+              _hover={{ backgroundColor: "gray.700" }}
+              display="flex"
+              alignItems="center"
+              gap={2}
+            >
+              <Icon as={JamMessagesF} />
+              <Text color="white" display={{ base: "none", lg: "inline" }}>
+                Chat
+              </Text>
+            </Box>
+          </Link>
+        </Tooltip>
+        <Tooltip label="FAQ" display={{ base: "initial", lg: "none" }}>
+          <Link as={NavLink} to="/faq" _activeLink={{ fontWeight: "900" }}>
+            <Box
+              paddingY={2}
+              paddingX={{ base: 0, sm: 2 }}
+              _hover={{ backgroundColor: "gray.700" }}
+              display="flex"
+              alignItems="center"
+              gap={2}
+            >
+              <Icon as={MdiFrequentlyAskedQuestions} />
+              <Text color="white" display={{ base: "none", lg: "inline" }}>
+                FAQ
+              </Text>
+            </Box>
+          </Link>
+        </Tooltip>
+      </Box>
+      <Box>
+        <Divider />
+        <Box width="100%">
+          {user ? (
+            <SidebarMenu user={user} />
+          ) : (
+            <Tooltip label="Sign in" display={{ base: "initial", lg: "none" }}>
+              <Link as={NavLink} to="/signIn" _activeLink={{ fontWeight: "900" }}>
+                <Box
+                  paddingY={2}
+                  paddingX={{ base: 0, sm: 2 }}
+                  _hover={{ backgroundColor: "gray.700" }}
+                  display="flex"
+                  alignItems="center"
+                  gap={2}
                 >
-                  <path
-                    fill="currentColor"
-                    d="M12 21v-2h7V5h-7V3h7q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21h-7Zm-2-4l-1.375-1.45l2.55-2.55H3v-2h8.175l-2.55-2.55L10 7l5 5l-5 5Z"
-                  />
-                </Icon>
-                <Text color="white" display={{ base: "none", lg: "inline" }}>
-                  Sign In
-                </Text>
-              </Box>
-            </Link>
-          </Tooltip>
-        )}
+                  <Icon
+                    viewBox="0 0 24 24"
+                    boxSize={10}
+                    color="white"
+                    margin={{ base: "0 auto", sm: "initial" }}
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M12 21v-2h7V5h-7V3h7q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21h-7Zm-2-4l-1.375-1.45l2.55-2.55H3v-2h8.175l-2.55-2.55L10 7l5 5l-5 5Z"
+                    />
+                  </Icon>
+                  <Text color="white" display={{ base: "none", lg: "inline" }}>
+                    Sign In
+                  </Text>
+                </Box>
+              </Link>
+            </Tooltip>
+          )}
+        </Box>
       </Box>
     </Flex>
   );
