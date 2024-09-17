589879260c492d4dd2c904ea30f57d1684bd9f00
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-09-17 14:40:35 +0200

    Improve ArticleDetails font sizes

--- src/components/ArticleDetails/index.tsx
+++ src/components/ArticleDetails/index.tsx
@@ -5,7 +5,7 @@ import IArticle from "../../types/article";
 function ArticleDetails({ article }: { article: IArticle }) {
   return (
     <>
-      <Text fontSize="lg" fontWeight="bold">
+      <Text fontSize="md" fontWeight="bold">
         {article.nickname}
       </Text>
       <Flex alignItems="center" gap={1}>
@@ -15,7 +15,7 @@ function ArticleDetails({ article }: { article: IArticle }) {
             d="M12 14q-.425 0-.713-.288T11 13q0-.425.288-.713T12 12q.425 0 .713.288T13 13q0 .425-.288.713T12 14Zm-4 0q-.425 0-.713-.288T7 13q0-.425.288-.713T8 12q.425 0 .713.288T9 13q0 .425-.288.713T8 14Zm8 0q-.425 0-.713-.288T15 13q0-.425.288-.713T16 12q.425 0 .713.288T17 13q0 .425-.288.713T16 14Zm-4 4q-.425 0-.713-.288T11 17q0-.425.288-.713T12 16q.425 0 .713.288T13 17q0 .425-.288.713T12 18Zm-4 0q-.425 0-.713-.288T7 17q0-.425.288-.713T8 16q.425 0 .713.288T9 17q0 .425-.288.713T8 18Zm8 0q-.425 0-.713-.288T15 17q0-.425.288-.713T16 16q.425 0 .713.288T17 17q0 .425-.288.713T16 18ZM5 22q-.825 0-1.413-.588T3 20V6q0-.825.588-1.413T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.588 1.413T19 22H5Zm0-2h14V10H5v10Z"
           />
         </Icon>
-        <Text>
+        <Text fontSize="md">
           {article.created_at.split("T")[0].split("-").reverse().join(".")}
         </Text>
       </Flex>
@@ -24,12 +24,12 @@ function ArticleDetails({ article }: { article: IArticle }) {
       </Heading>
       {article.tags && article.tags.length > 0 && (
         <Stack direction="row" flexWrap="wrap" marginTop={4}>
-          <TagList tags={article.tags.slice(0, 8)} size="md" />
+          <TagList tags={article.tags.slice(0, 8)} size="sm" />
           {article.tags.length > 8 && (
             <Tag
               colorScheme="blue"
               _hover={{ bg: "#2B6CB0", color: "white" }}
-              size="md"
+              size="sm"
             >
               and more...
             </Tag>
