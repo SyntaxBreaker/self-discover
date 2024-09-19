ac9d25640494eacf0ce38f004ae0961c987b93ed
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-09-19 15:36:01 +0200

    Add maxWidth to text in EmptyChat

--- src/components/EmptyChat/index.tsx
+++ src/components/EmptyChat/index.tsx
@@ -5,7 +5,7 @@ function EmptyChat() {
   return (
     <Stack alignItems="center">
       <Image boxSize="250px" src={chattingImage} alt="" />
-      <Text textAlign="center" color="gray.600">
+      <Text textAlign="center" color="gray.600" maxWidth={600}>
         Welcome to the general chat! It is a place for everyone to discuss
         anything and everything. Please be respectful of other members.
       </Text>
