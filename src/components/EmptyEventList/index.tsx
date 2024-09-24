d64e462d613c992a0d300ac47eb264fcc643a0b1
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-09-24 14:08:32 +0200

    Add auth, button, and routing to EmptyEventList

--- src/components/EmptyEventList/index.tsx
+++ src/components/EmptyEventList/index.tsx
@@ -1,8 +1,12 @@
-import { Image, Text, Button, Flex, Heading } from "@chakra-ui/react";
+import { Image, Text, Button, Flex, Heading, Box } from "@chakra-ui/react";
 import eventImage from "../../assets/images/event.svg";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthProvider";
+import { IAuthContext } from "../../types/auth";
 
 function EmptyEventList() {
+  const { user } = useAuth() as IAuthContext;
+
   return (
     <Flex flexDirection="column" gap={2} alignItems="center">
       <Image boxSize="250px" src={eventImage} alt="" />
@@ -17,15 +21,11 @@ function EmptyEventList() {
       <Text textAlign="center" color="gray.600" fontSize="sm">
         There are no events yet. You can add a new event using the button below.
       </Text>
-      <Button
-        size="md"
-        colorScheme="facebook"
-        as={Link}
-        to={`create`}
-        marginTop={2}
-      >
-        Create
-      </Button>
+      <Box as={Button} colorScheme="facebook" textAlign="center" marginTop={4}>
+        <Link to={user ? "/events/create" : "/signIn"}>
+          {user ? "Add event" : "Sign in"}
+        </Link>
+      </Box>
     </Flex>
   );
 }
