f268735999d30000595d8251c09e4c94efcf2b5e
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-09-24 14:01:43 +0200

    Add auth, button, and routing to EmptyTagList

--- src/components/EmptyTagList/index.tsx
+++ src/components/EmptyTagList/index.tsx
@@ -1,19 +1,32 @@
-import { Image, Heading, Text, Flex } from "@chakra-ui/react";
+import { Image, Heading, Text, Flex, Button, Box } from "@chakra-ui/react";
 import searchingImage from "../../assets/images/searching.svg";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthProvider";
+import { IAuthContext } from "../../types/auth";
 
 function EmptyTagList() {
+  const { user } = useAuth() as IAuthContext;
+
   return (
     <Flex direction="column" gap={2} marginTop={8}>
       <Image src={searchingImage} alt="" boxSize="sm" alignSelf="center" />
       <Heading
-        fontSize="lg"
+        fontSize="xl"
         textAlign="center"
         fontWeight="bold"
         color="gray.700"
       >
-        Oops! There are currently no tags associated with this keyword.
+        Oops! It&apos;s empty here.
       </Heading>
-      <Text fontSize="sm" textAlign="center" color="gray.600"></Text>
+      <Text textAlign="center" color="gray.600" fontSize="sm" maxWidth={600}>
+        There are currently no tags associated with this keyword. You can add a
+        new article using the button below.
+      </Text>
+      <Box as={Button} colorScheme="facebook" textAlign="center" marginTop={4}>
+        <Link to={user ? "/create" : "/signIn"}>
+          {user ? "Add Article" : "Sign in"}
+        </Link>
+      </Box>
     </Flex>
   );
 }
