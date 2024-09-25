e11d0db1e4ea28c15db1bcae04ca05b7f5137a73
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-09-25 14:18:53 +0200

    Improve font sizes in ArticleStats

--- src/components/ArticleStats/index.tsx
+++ src/components/ArticleStats/index.tsx
@@ -7,14 +7,14 @@ function ArticleStats({ article }: { article: IArticle }) {
     <Stack direction="row" spacing="24px">
       <Flex alignItems="center" gap={2}>
         <Icon as={AkarIconsThumbsUp} />
-        <Text fontSize="sm">
+        <Text fontSize="xs">
           {article.likes && article.likes.length > 0 ? article.likes.length : 0}{" "}
           {article.likes?.length === 1 ? "like" : "likes"}
         </Text>
       </Flex>
       <Flex alignItems="center" gap={2}>
         <Icon as={Fa6RegularComments} />
-        <Text fontSize="sm">
+        <Text fontSize="xs">
           {article.comments && article.comments.length > 0
             ? article.comments.length
             : 0}{" "}
